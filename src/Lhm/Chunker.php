01cafaf3a98bbb3e8d7c2d39c8e0b5d6d42de6c9
Mark Caudill <mark.caudill@sharpspring.com>
2015-10-31 16:43:34 -0400

    Added support for skipping keys inside the chunker and increasing stride if not very busy

--- src/Lhm/Chunker.php
+++ src/Lhm/Chunker.php
@@ -3,6 +3,7 @@
 namespace Lhm;
 
 use Phinx\Db\Adapter\AdapterInterface;
+use PDO;
 
 
 class Chunker extends Command
@@ -64,7 +65,11 @@ class Chunker extends Command
         $this->destination = $destination;
         $this->sqlHelper = $sqlHelper ?: new SqlHelper($this->adapter);
 
-        $this->options = $options + ['stride' => 2000];
+        $this->options = $options + [
+                'stride' => 2000,
+                'strideMax' => 20000000,
+                'targetQuerySeconds' => 0.75
+            ];
 
         $this->primaryKey = $this->adapter->quoteColumnName($this->sqlHelper->extractPrimaryKey($this->origin));
 
@@ -76,21 +81,87 @@ class Chunker extends Command
 
     protected function execute()
     {
-
         $this->getLogger()->info("Copying data from `{$this->origin->getName()}` into `{$this->destination->getName()}`");
 
+        $stride = $this->options['stride'];
+        $strideMax = $this->options['strideMax'];
+        $targetQuerySeconds = $this->options['targetQuerySeconds'];
+        if (empty($targetQuerySeconds)) {
+            $targetQuerySeconds = 0.75;
+        }
+        $weightedAvgPace = 0;
 
-        while ($this->nextToInsert < $this->limit || ($this->nextToInsert == 1 && $this->start == 1)) {
-
-            $query = $this->copy($this->bottom(), $this->top($this->options['stride']));
-
-            $this->getLogger()->debug($query);
-
-            $this->adapter->query($query);
-            $this->nextToInsert = $this->top($this->options['stride']) + 1;
+        // if we have fewer rows than the stride, just copy it all in one pass
+        $estRows = $this->getEstimatedRows();
+        if ($estRows < $stride) {
+            $stride = $this->limit;
         }
 
+        $strideStart = $stride;
+        $initialStart = microtime(true);
+        $lastUpdate = 0;
+        $totalRows = 0;
+        while ($this->nextToInsert < $this->limit || ($this->nextToInsert == 1 && $this->start == 1)) {
+            $top = $this->top($stride);
+            $bottom = $this->bottom();
+            $queries = $this->copy($bottom, $top);
 
+            $query = "$queries[0] $queries[1]";
+            $this->getLogger()->debug($query);
+
+            $startTime = microtime(true);
+            $result = $this->adapter->query($query);
+            $rowCount = $result->rowCount();
+            $totalRows += $rowCount;
+            $endTime = microtime(true);
+            $timing = $endTime - $startTime;
+            $this->nextToInsert = $top + 1;
+
+            if ($endTime - $lastUpdate > 10) {
+                $msg = "Copied $totalRows of an estimated $estRows rows.\n";
+                $this->getLogger()->info($msg);
+                $msg = "There is a stride of $stride and a goal of $targetQuerySeconds.";
+                $this->getLogger()->info($msg);
+                $lastUpdate = $endTime;
+            }
+
+            if ($rowCount === 0) {
+                // find the next row
+                $result = $this->adapter->query($queries[2]);
+                $row = $result->fetch(PDO::FETCH_NUM);
+                if (!empty($row) && !empty($row[0])) {
+                    $this->nextToInsert = $row[0];
+                }
+            } else {
+                $rowsPerSec = $rowCount / $timing;
+                $trendWeight = (0.5 * ($rowsPerSec - $weightedAvgPace));
+                $weightedAvgPace = $weightedAvgPace + $trendWeight;
+                if (!empty($weightedAvgPace)) {
+                    $targetStride = $targetQuerySeconds * $weightedAvgPace;
+                    $ratio =  max(min($targetStride / $stride, 1.25), 0.5);
+                    $stride = $ratio * $stride;
+                    $stride = max(min($stride, $strideMax), $strideStart);
+                    $stride = (int)floor($stride);
+                }
+            }
+        }
+
+        $totalTime = microtime(true) - $initialStart;
+        $this->getLogger()->info("Copied $totalRows in $totalTime.\n");
+    }
+
+    /**
+     * @return int
+     */
+    protected function getEstimatedRows()
+    {
+        $originName = $this->adapter->quoteTableName($this->origin->getName());
+
+        $sql = "EXPLAIN SELECT * FROM $originName WHERE 1";
+        $result = $this->adapter->query($sql);
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+
+        return $row['rows'];
     }
 
     protected function top($stride)
@@ -142,11 +213,15 @@ class Chunker extends Command
             )
         );
 
-
-        return implode(" ", [
+        $queries = array(
             "INSERT IGNORE INTO {$destinationName} ({$destinationColumns})",
-            "SELECT {$originColumns} FROM {$originName}",
-            "WHERE {$originName}.{$this->primaryKey} BETWEEN {$lowest} AND {$highest}"
-        ]);
+            "SELECT {$originColumns} FROM {$originName}
+             WHERE {$originName}.{$this->primaryKey} BETWEEN {$lowest} AND {$highest}",
+            "SELECT {$originName}.{$this->primaryKey} FROM {$originName}
+             WHERE {$originName}.{$this->primaryKey} >= {$lowest}
+             LIMIT 1",
+        );
+
+        return $queries;
     }
 }
